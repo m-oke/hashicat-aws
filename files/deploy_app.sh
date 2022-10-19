97ee522950ec7f7e05cdfac7ae22a80f87806adc
errorcode <errorcode.sc2@gmail.com>
2022-10-19 15:14:15 +0900

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  This is ACME's Wonderlands!!!
+  This is ACME's Wonderland!!!
   <!-- END -->
 
   </div>
